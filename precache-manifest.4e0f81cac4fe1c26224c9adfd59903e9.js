5a4043155d112d3ff5972444898c22107dbb888c
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2021-02-19 14:38:07 -0800

    Updates

---
+++ precache-manifest.4e0f81cac4fe1c26224c9adfd59903e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a70d7d9ef069a87dbff7f0263234258",
+    "url": "/Deploy-react-app/index.html"
+  },
+  {
+    "revision": "904ecbdf499df37d246d",
+    "url": "/Deploy-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cf6b9d126e0df03d2b0",
+    "url": "/Deploy-react-app/static/js/2.152314b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Deploy-react-app/static/js/2.152314b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904ecbdf499df37d246d",
+    "url": "/Deploy-react-app/static/js/main.187ddfd2.chunk.js"
+  },
+  {
+    "revision": "f451805f404989ec71e7",
+    "url": "/Deploy-react-app/static/js/runtime-main.cd5a7004.js"
+  }
+]);
